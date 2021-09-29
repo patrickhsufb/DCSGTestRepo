1378459aa3fa0087e96e6da141d375a1ce86a795
patrickhsu <patrickhsu@fb.com>
2021-09-28 17:07:39 -0700

    add socks

--- pytests/pytest_sample_access.py
+++ pytests/pytest_sample_access.py
@@ -1,4 +1,5 @@
 import os
+from python_socks.sync import Proxy
 
 def inc(x):
     return x + 1
@@ -6,4 +7,5 @@ def inc(x):
 
 def test_answer():
     os.mkdir("potatopath")
+    proxy = Proxy.from_url('socks5://user:password@127.0.0.1:1080')
     assert inc(3) == 4
